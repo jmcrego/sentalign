85856e68dc4319f0455e419df1c6152c9c68316f
jmcrego <jmcrego@gmail.com>
2020-02-24 22:02:19 +0100

    trainer

--- src/trainer.py
+++ src/trainer.py
@@ -143,7 +143,6 @@ class Trainer():
                 batch_loss_mlm = self.computeloss_mlm(h_xy, xy_refs)
                 loss_mlm = batch_loss_mlm / npred_mlm
                 loss += self.step_mlm['w'] * loss_mlm
-                print(loss)
 
             if self.step_ali['w'] > 0.0 and False: ### (ALI)
                 h_xy = self.model.forward(xy, mask_xy)
