fe208bc40cb4ac2d3ea720e1e144929e0409a055
jmcrego <jmcrego@gmail.com>
2020-02-27 16:53:03 +0100

    cos

--- src/infer.py
+++ src/infer.py
@@ -97,12 +97,12 @@ class Infer():
                     t = h_xy[:,ls+1,:] # take embedding of <sep>
                 else:
                     logging.error('bad pooling method: {}'.format(self.pooling))
-                s = F.normalize(s,p=2,dim=1,eps=1e-12).unsqueeze(-2)
-                t = F.normalize(t,p=2,dim=1,eps=1e-12).unsqueeze(-2)
+                s = F.normalize(s,p=2,dim=1,eps=1e-12).unsqueeze(-2) #[bs, 1, es]
+                t = F.normalize(t,p=2,dim=1,eps=1e-12).unsqueeze(-1) #[bs, es, 1]
                 print('s',s.shape)
                 print('t',t.shape)
 #                sim = F.cosine_similarity(s, t, dim=1, eps=1e-12).cpu().detach().numpy()
-                DP = torch.bmm(s, torch.transpose(t, 2, 1)).squeeze()
+                DP = torch.bmm(s, t)
                 print('DP',DP.shape)
                 ### output
                 if self.matrix:
